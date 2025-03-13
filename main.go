279b75e1c9eeb6f5b0cc8b74266e7d3488d4ace0
herzorf <1446450047@qq.com>
2025-03-13 14:24:42 +0800

    feat: 添加简单的文件并打包

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// 创建一个默认的 Gin 路由
+	router := gin.Default()
+
+	// 定义 /ping 接口
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pang")
+	})
+
+	// 定义 /fuck 接口
+	router.GET("/fuck", func(c *gin.Context) {
+		c.String(http.StatusOK, "motherfuker")
+	})
+
+	// 启动服务器，监听 8080 端口
+	router.Run(":8080")
 }
